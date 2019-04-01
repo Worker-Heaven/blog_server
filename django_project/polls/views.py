be6fe5e8c1f13af8f91e37f72a76c2ffa3f3905a
Worker-Heaven <superstar.geekboy@gmail.com>
2019-04-01 00:30:40 -0300

    Css styling

--- django_project/polls/views.py
+++ django_project/polls/views.py
@@ -2,23 +2,32 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
+from django.views import generic
+
 # from django.template import loader
 
 from .models import Question, Choice
 
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
 
-    context = {
-        'latest_question_list': latest_question_list,
-    }
-
-    return render(request, 'polls/index.html', context)
+    def get_queryset(self):
+        return Question.objects.order_by('-pub_date')[:5]
 
 
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/detail.html', {'question': question})
+    # latest_question_list = Question.objects.order_by('-pub_date')[:5]
+
+    # context = {
+    #     'latest_question_list': latest_question_list,
+    # }
+
+    # return render(request, 'polls/index.html', context)
+
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
 
 def results(request, question_id):
     response = "You are looking at the results of question %s"
